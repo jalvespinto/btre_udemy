d62c93d13742c34be107f190883df14deba56284
jalvespinto <joaquim.alves@peerdustry.com>
2021-09-19 11:10:48 -0300

    ready to deploy

--- btre/settings.py
+++ btre/settings.py
@@ -36,12 +36,17 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -80,8 +85,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('btredb_NAME'),
+        'USER': env('btredb_USER'),
+        'PASSWORD': env('btredb_PASS'),
+        'HOST': 'localhost'
     }
 }
 
@@ -130,3 +138,21 @@ STATICFILES_DIRS = [BASE_DIR / 'btre/static']
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Folder Settings
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_PASS')
+EMAIL_USE_TLS=True
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,32 @@
+from typing import ContextManager
+from django.contrib.admin.filters import RelatedFieldListFilter
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from listings.models import Listing
+from realtors.models import Realtor
+from listings.choices import bedroom_choices,price_choices,state_choices
 
 def index(request):
-    return render(request, 'pages/index.html')
+
+    listings = Listing.objects.filter(is_published=True).order_by("-list_date")[:3]
+
+    context = {
+        'latest_listings': listings,
+        'bedrooms_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'state_choices': state_choices
+    }
+
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+
+    realtors = Realtor.objects.order_by("-hire_date")
+    mvp = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'mvp': mvp,
+        'realtors': realtors
+    }
+
+    return render(request, 'pages/about.html', context)
